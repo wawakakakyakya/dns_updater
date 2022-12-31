b3ebd40dc17ab893f5f54e159506207cff16adb4
wawakakakyakya <wawakakakyakya@yahoo.co.jp>
2022-12-31 22:03:12 +0900

    refs #5 set version

--- config/args.go
+++ config/args.go
@@ -2,11 +2,25 @@ package config
 
 import (
 	"flag"
+	"fmt"
+	"os"
+
+	"dns_updater/version"
 )
 
-var configPath string
+var (
+	configPath string = ""
+	showVersion bool = false
+)
 
 func init() {
 	flag.StringVar(&configPath, "config", "config.yml", "config path, default ./config.yml")
+	flag.BoolVar(&showVersion, "v", false, "show application version")
+	flag.BoolVar(&showVersion, "version", false, "show application version")
 	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("version(%s.%s)\n", version.Version, version.Revision)
+		os.Exit(0)
+	}
 }

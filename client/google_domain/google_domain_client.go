efceb12cbec29ea9870a8b864060d712f97d7109
boiledegg <wawakawkakyakya@yahoo.co.jp>
2023-01-04 00:11:11 +0900

    refs #8 fix update google domain fail

--- client/google_domain/google_domain_client.go
+++ client/google_domain/google_domain_client.go
@@ -2,12 +2,16 @@ package googledomain
 
 import (
 	ddnsclient "dns_updater/client/ddns_client"
-	"dns_updater/logger"
-
 	"dns_updater/config"
+	"dns_updater/logger"
+	"errors"
+	"regexp"
 )
 
-var googleDomainURL string = "https://domains.google.com/nic/update"
+var (
+	googleDomainURL string = "https://domains.google.com/nic/update"
+	okResp                 = regexp.MustCompile(`^(good|nochg)\s[0-9\.]*$`)
+)
 
 type GoogleDomainClient struct {
 	ddnsclient *ddnsclient.DDNSClient
@@ -26,12 +30,18 @@ func (m *GoogleDomainClient) Add() error {
 func (m *GoogleDomainClient) Update(errCh chan<- error) {
 	m.logger.Debug("update called")
 	m.logger.InfoF("update google domain with %s will start", m.Name)
-	_, err := m.ddnsclient.Update()
-	if err != nil {
+	resp, err := m.ddnsclient.Update()
+	if respS := resp.String(); !okResp.MatchString(resp.String()) {
+		m.logger.ErrorF("update with %s failed", m.Name)
+		err = errors.New(respS)
+		errCh <- err
+	} else if err != nil {
 		m.logger.ErrorF("update with %s failed", m.Name)
 		errCh <- err
+	} else {
+		m.logger.InfoF("update with %s was executed successfully", m.Name)
 	}
-	m.logger.InfoF("update with %s was executed successfully", m.Name)
+
 }
 
 func NewGoogleDomainClient(cfg *config.YamlConfig, logger *logger.Logger) *GoogleDomainClient {
@@ -44,5 +54,5 @@ func NewGoogleDomainClient(cfg *config.YamlConfig, logger *logger.Logger) *Googl
 		return nil
 	}
 	ddnsclient.SetParam("hostname", cfg.GoogleDomain.Name)
-	return &GoogleDomainClient{ddnsclient: ddnsclient, logger: googleDomainLogger, Name: cfg.GoogleDomain.UserName}
+	return &GoogleDomainClient{ddnsclient: ddnsclient, logger: googleDomainLogger, Name: cfg.GoogleDomain.Name}
 }
